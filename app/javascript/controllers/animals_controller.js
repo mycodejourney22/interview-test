99e00a02d113dac96cc22b6f1104ba3cf54bed77
afeez oloyede <oloyedeafeez@363photography.org>
2022-07-13 12:19:47 -0700

    Added button for refresh

--- app/javascript/controllers/animals_controller.js
+++ app/javascript/controllers/animals_controller.js
@@ -3,8 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="animals"
 export default class extends Controller {
   static targets = ["listAnimal"]
+
   connect() {
-    console.log(this.listAnimalTarget)
+    this.#getanimal()
+  }
+
+  refreshnew (event){
+    window.location.reload();
+  }
+
+
+
+
+  #getanimal(){
     const url = "https://zoo-animal-api.herokuapp.com/animals/rand/5"
     fetch(url)
       .then(response => response.json())
@@ -12,11 +23,10 @@ export default class extends Controller {
         data.forEach(animal => {
         const myList = `<li class="list-inline-item">
                           <img src="${animal.image_link}" width="600" height="400" alt="${animal.animal_type} Picture" />
-                          <p>Name: ${animal.name}  Type: ${animal.animal_type}  Habitat: ${animal.habitat}</p>
+                          <p>Name: ${animal.name}  Type: ${animal.animal_type}</p>
                         </li>`
         this.listAnimalTarget.insertAdjacentHTML("beforeend",myList )
         });
       })
-
   }
 }
