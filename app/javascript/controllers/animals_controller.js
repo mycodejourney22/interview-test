fe850f02b0304ef0cfd28946d4591e0476ba9eb4
afeez oloyede <oloyedeafeez@363photography.org>
2022-07-13 16:48:15 -0700

    added favorites

--- app/javascript/controllers/animals_controller.js
+++ app/javascript/controllers/animals_controller.js
@@ -2,18 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="animals"
 export default class extends Controller {
-  static targets = ["listAnimal"]
+  static targets = ["listAnimal", "favorites"]
 
   connect() {
+    console.log("connected to the animals controller")
     this.#getanimal()
   }
 
   refreshnew (event){
     window.location.reload();
   }
-
-
-
 
   #getanimal(){
     const url = "https://zoo-animal-api.herokuapp.com/animals/rand/5"
@@ -22,11 +20,23 @@ export default class extends Controller {
       .then(data => {
         data.forEach(animal => {
         const myList = `<li class="list-inline-item">
-                          <img src="${animal.image_link}" width="600" height="400" alt="${animal.animal_type} Picture" />
+                          <div class="card-category" style="background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${animal.image_link})">
+                          <div class= "favorite"><span id="img-icon" data-animals-target="favorites" data-action="click->animals#favoriteclick"><i class="fa-solid fa-heart"></i></span></div>
+                         </div>
                           <p>Name: ${animal.name}  Type: ${animal.animal_type}</p>
                         </li>`
         this.listAnimalTarget.insertAdjacentHTML("beforeend",myList )
         });
       })
   }
+
+  favoriteclick(event) {
+    event.currentTarget.classList.toggle("img-favorites")
+    const formparent = event.currentTarget.parentElement
+    const myParent = formparent.parentElement
+    const rightParent = myParent.parentElement
+    console.log(rightParent)
+  }
+
+
 }
