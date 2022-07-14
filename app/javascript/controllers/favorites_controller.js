ec8f9f055cab2ec0e7c63961c16f0d3424c0fcd7
afeez oloyede <oloyedeafeez@363photography.org>
2022-07-14 01:38:22 -0700

    added a message to favorite page if there is no list

--- app/javascript/controllers/favorites_controller.js
+++ app/javascript/controllers/favorites_controller.js
@@ -5,13 +5,19 @@ export default class extends Controller {
   static targets = ["myFavorite"]
   connect() {
     const arr = []
-    for (let i = 0; i < localStorage.length; i++) {
-      const key = localStorage.key(i);
-      arr.push(localStorage.getItem(key));
+    if (localStorage.length > 1) {
+      for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        arr.push(localStorage.getItem(key));
+      }
+      arr.forEach((list) => {
+        this.myFavoriteTarget.insertAdjacentHTML("beforeend",list)
+      })
     }
-    arr.forEach((list) => {
-      this.myFavoriteTarget.insertAdjacentHTML("beforeend",list)
-    })
+    else {
+      const noList = "<h2>Go back and add your favorite animal</h2>"
+      this.myFavoriteTarget.insertAdjacentHTML("beforeend",noList)
+    }
   }
 
 }
